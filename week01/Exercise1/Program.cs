7ea7ef27213335bb0152e27feaa7a5dba41362a1
lwande <lwandedana01@gmail.com>
2026-07-04 19:47:09 +0200

    program.cs first code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
     }
 }
