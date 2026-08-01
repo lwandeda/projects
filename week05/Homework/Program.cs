6a33b560f5c5feb4dcfe2b448dc6e7b9803896fa
Lwande Dana <lwandedana01@gmail.com>
2026-08-01 19:36:51 +0200

    week 5 inheritence project

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,24 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment(
+            "Lwande Dana",
+            "Fractions",
+            "7.3",
+            "8-19");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment(
+            "Lwande Dana",
+            "European History",
+            "The Causes of World War II");
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
