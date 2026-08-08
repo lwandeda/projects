72600e28de117b78f602a2e15e39ee47c5ddec76
Lwande Dana <lwandedana01@gmail.com>
2026-08-08 19:49:17 +0200

    Polymorphism program week6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 5);
+
+        Console.WriteLine(
+            $"Square: {square.GetColor()} - Area: {square.GetArea()}"
+        );
+
+
+        Rectangle rectangle = new Rectangle("Blue", 6, 4);
+
+        Console.WriteLine(
+            $"Rectangle: {rectangle.GetColor()} - Area: {rectangle.GetArea()}"
+        );
+
+
+        Circle circle = new Circle("Green", 3);
+
+        Console.WriteLine(
+            $"Circle: {circle.GetColor()} - Area: {circle.GetArea()}"
+        );
+
+
+        Console.WriteLine();
+        Console.WriteLine("Polymorphism with a list:");
+        Console.WriteLine();
+
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Yellow", 4));
+
+        shapes.Add(new Rectangle("Purple", 5, 3));
+
+        shapes.Add(new Circle("Orange", 2));
+
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(
+                $"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}"
+            );
+        }
     }
 }
